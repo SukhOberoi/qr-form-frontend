d43fa3c29f7ef07324db4a905267078f3f61f6ba
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2024-08-11 01:38:28 +0530

    Fix response handling

--- src/Form.jsx
+++ src/Form.jsx
@@ -82,7 +82,7 @@ const Form = () => {
         },
       );
 
-      const result = response.json();
+      const result = await response.json();
       if (!response.ok) {
         throw new Error(result.message);
       }

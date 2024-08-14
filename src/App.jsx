b565f31ccf5d95ab24093389e156b33835cf01b2
Sukh Oberoi <sukhoberoi24@gmail.com>
2024-08-14 13:38:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Closed from './Closed.jsx'
 
 export default function App() {
     const currentTime = new Date();
-    const closeTime = new Date('2024-08-13T09:30:00Z');
+    const closeTime = new Date('2024-08-14T09:30:00Z');
     const formOpen = currentTime < closeTime
   return (
     <>
